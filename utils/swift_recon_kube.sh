d6bd2453d1ae71612261d28b5a2e1c82ff4a194c
Stefan Majewsky <stefan.majewsky@sap.com>
2017-04-10 10:22:16 +0200

    add utils

---
+++ utils/swift_recon_kube.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec kubectl --namespace=swift exec "$(kubectl --namespace=swift get pods -l component=swift-proxy-cluster-3 --no-headers | awk '{print$1}' | head -n1)" -- swift-recon "$@"
