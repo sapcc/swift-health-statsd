1473393a90e9a9c4459c04b0c0d505236c3b68e7
Stefan Majewsky <stefan.majewsky@sap.com>
2018-04-20 13:51:02 +0200

    adjust for current helm-charts

--- utils/swift_dispersion_kube.sh
+++ utils/swift_dispersion_kube.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec kubectl --namespace=swift exec "$(kubectl --namespace=swift get pods -l component=swift-proxy-cluster-3 --no-headers | awk '{print$1}' | head -n1)" -- swift-dispersion-report "$@"
+exec kubectl --namespace=swift exec -c proxy "$(kubectl --namespace=swift get pods -l component=swift-proxy-cluster-3 --no-headers | awk '{print$1}' | head -n1)" -- swift-dispersion-report "$@"
